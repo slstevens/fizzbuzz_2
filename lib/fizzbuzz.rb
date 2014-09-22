92e03511a9d5c3ab0aa9cc674e4eecab856dcf04
slstevens <sean.stevens.123@gmail.com>
2014-09-22 13:29:55 +0100

    Added divisible by 3 method

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+class FizzBuzz
+	def is_divisible_by_3?(number)
+		number % 3 == 0
+	end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,10 @@
 require 'fizzbuzz.rb'
 
 describe FizzBuzz do
+
+	let (:fizzbuzz) {FizzBuzz.new}
 	
+	it "should know if it is divisible by 3" do
+		expect(fizzbuzz.is_divisible_by_3?(3)).to eq true
+	end
 end
