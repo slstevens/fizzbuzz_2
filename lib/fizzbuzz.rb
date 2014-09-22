134b337e09b7391ecb37a98288b28443357ed762
slstevens <sean.stevens.123@gmail.com>
2014-09-22 13:53:32 +0100

    Refactored fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -16,12 +16,8 @@ class FizzBuzz
 	end
 
 	def fizzbuzz(number)
-		if is_divisible_by_15?(number)
-			return "FizzBuzz"
-		elsif is_divisible_by_3?(number)
-			return "Fizz"
-		elsif is_divisible_by_5?(number)
-			return "Buzz"
-		end
+		return "FizzBuzz" if is_divisible_by_15?(number)
+		return "Fizz" if is_divisible_by_3?(number)
+		return "Buzz" if is_divisible_by_5?(number)
 	end
 end
