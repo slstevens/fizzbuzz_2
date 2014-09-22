bfb46da9e3e5f61fdb122135a6ccd1a625fabb17
slstevens <sean.stevens.123@gmail.com>
2014-09-22 13:34:24 +0100

    Added divisible by 5 an 15 test and methods

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,4 +2,12 @@ class FizzBuzz
 	def is_divisible_by_3?(number)
 		number % 3 == 0
 	end
+
+	def is_divisible_by_5?(number)
+		number % 5 == 0
+	end
+
+	def is_divisible_by_15?(number)
+		number % 15 == 0 
+	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,20 @@ describe FizzBuzz do
 	it "should know if it is divisible by 3" do
 		expect(fizzbuzz.is_divisible_by_3?(3)).to eq true
 	end
+
+	it "should know if it is not divisible by 3" do
+		expect(fizzbuzz.is_divisible_by_3?(4)).to eq false
+	end
+
+	it "should know if it is diviible by 5" do
+		expect(fizzbuzz.is_divisible_by_5?(5)).to eq true
+	end
+
+	it "should know if it is not divisible by 5" do
+		expect(fizzbuzz.is_divisible_by_5?(1)).to eq false
+	end
+
+	it "should know if it is divisible by 3 and 5" do
+		expect(fizzbuzz.is_divisible_by_15?(15)).to eq true
+	end
 end
