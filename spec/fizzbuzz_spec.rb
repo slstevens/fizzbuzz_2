d731d40f4cd97ca74977e226a8918f58737208a8
slstevens <sean.stevens.123@gmail.com>
2014-09-22 13:38:30 +0100

    Added contexts to spec file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,23 +4,34 @@ describe FizzBuzz do
 
 	let (:fizzbuzz) {FizzBuzz.new}
 	
-	it "should know if it is divisible by 3" do
-		expect(fizzbuzz.is_divisible_by_3?(3)).to eq true
+	context "it is divisible by" do
+
+		it "should know if it is divisible by 3" do
+			expect(fizzbuzz.is_divisible_by_3?(3)).to eq true
+		end
+
+		it "should know if it is divisible by 5" do
+			expect(fizzbuzz.is_divisible_by_5?(5)).to eq true
+		end
+
+		it "should know if it is divisible by 3 and 5" do
+			expect(fizzbuzz.is_divisible_by_15?(15)).to eq true
+		end
 	end
 
-	it "should know if it is not divisible by 3" do
-		expect(fizzbuzz.is_divisible_by_3?(4)).to eq false
-	end
 
-	it "should know if it is diviible by 5" do
-		expect(fizzbuzz.is_divisible_by_5?(5)).to eq true
-	end
+	context "it is not divisible by" do
+	
+		it "should know if it is not divisible by 3" do
+			expect(fizzbuzz.is_divisible_by_3?(4)).to eq false
+		end
+		
+		it "should know if it is not divisible by 5" do
+			expect(fizzbuzz.is_divisible_by_5?(1)).to eq false
+		end
 
-	it "should know if it is not divisible by 5" do
-		expect(fizzbuzz.is_divisible_by_5?(1)).to eq false
-	end
-
-	it "should know if it is divisible by 3 and 5" do
-		expect(fizzbuzz.is_divisible_by_15?(15)).to eq true
+		it "should know if it is not divisible by 3 and 5" do
+			expect(fizzbuzz.is_divisible_by_15?(1 )).to eq false
+		end
 	end
 end
